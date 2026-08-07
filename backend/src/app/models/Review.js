a7bd64b073e197d905523b2caa3a853bcad48794
Thanh Tan <tanden1357@gmail.com>
2026-08-07 15:43:37 +0700

    fixed model review

--- backend/src/app/models/Review.js
+++ backend/src/app/models/Review.js
@@ -4,14 +4,20 @@ const { Schema } = mongoose
 const ReviewSchema = new Schema(
   {
     product_id: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    variant_id: {
+      type: Schema.Types.ObjectId,
+      ref: "ProductVariant",
+      required: true,
+    },
+
     user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
     rating: { type: Number, required: true, min: 1, max: 5 },
     comment: { type: String },
   },
-  { timestamps: { createdAt: "created_at", updatedAt: false } }
+  { timestamps: { createdAt: "created_at", updatedAt: false } },
 )
 
 ReviewSchema.index({ product_id: 1 })
 ReviewSchema.index({ user_id: 1 })
-
+ReviewSchema.index({ variant_id: 1 })
 module.exports = mongoose.model("Review", ReviewSchema)
