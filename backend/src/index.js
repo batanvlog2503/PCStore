9c1a737448298ec63ef2c384a6dbf2b27a856d88
Thanh Tan <tanden1357@gmail.com>
2026-09-22 11:25:37 +0700

    fix cors 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -48,7 +48,7 @@ app.use(morgan("combined"))
 app.use(express.static(path.join(__dirname, "public")))
 console.log("Static path:", path.join(__dirname, "public"))
 // Template engine setup
-app.use(cors())
+app.use(cors("*"))
 app.use(express.urlencoded({ extended: true })) // đọc form
 app.use(express.json()) // cho phép đọc json
 
